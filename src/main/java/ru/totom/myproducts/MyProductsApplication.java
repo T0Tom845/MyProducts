af601638ee51e6475108206fa6b21671ea98e3c5
Kirill Lapin <lapinkirill.croc@mail.ru>
2024-11-14 12:18:37 +0300

    initial commit

--- src/main/java/ru/totom/myproducts/MyProductsApplication.java
+++ src/main/java/ru/totom/myproducts/MyProductsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MyProductsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyProductsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyProductsApplication.class, args);
+    }
 
 }
